6fc128e35144804404201bbe417e46dd188cfe33
Muhammad Junaid <mjunaidyousafzai948@gmail.com>
2025-03-10 20:29:01 +0000

    all fixed

--- src/app/components/Bg.jsx
+++ src/app/components/Bg.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
@@ -7,12 +7,55 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Bg = () => {
   const bgRef = useRef(null);
+  const [bubbles, setBubbles] = useState([]);
 
   useEffect(() => {
+    // Generate random bubbles only on the client side
+    const createBubbles = () => {
+      const bubbles = [];
+      const bubbleCount = 4;
+
+      for (let i = 0; i < bubbleCount; i++) {
+        const size = gsap.utils.random(300, 600);
+        const top = gsap.utils.random(-50, 150) + "vh";
+        const left = gsap.utils.random(-20, 120) + "vw";
+        const rotation = gsap.utils.random(-45, 45);
+
+        const gradientOptions = [
+          "bg-gradient-to-tl from-teal-700 to-black",
+          "bg-gradient-to-br from-teal-800 to-black",
+          "bg-gradient-to-br from-teal-600 to-black",
+          "bg-teal-900",
+          "bg-teal-500",
+        ];
+        const gradient = gsap.utils.random(gradientOptions);
+
+        bubbles.push(
+          <div
+            key={i}
+            className={`absolute blob ${gradient}`}
+            style={{
+              width: `${size}px`,
+              height: `${size}px`,
+              top: top,
+              left: left,
+              filter: `blur(${gsap.utils.random(120, 250)}px)`,
+              opacity: gsap.utils.random(0.3, 0.6),
+              transform: `rotate(${rotation}deg)`,
+            }}
+          ></div>
+        );
+      }
+
+      return bubbles;
+    };
+
+    setBubbles(createBubbles());
+
+    // GSAP animations
     const blobs = gsap.utils.toArray(".blob");
 
     blobs.forEach((blob) => {
-   
       gsap.to(blob, {
         x: gsap.utils.random(-150, 150),
         y: gsap.utils.random(-150, 150),
@@ -21,7 +64,6 @@ const Bg = () => {
         yoyo: true,
         ease: "power1.inOut",
       });
-
 
       gsap.fromTo(
         blob,
@@ -40,54 +82,13 @@ const Bg = () => {
     });
   }, []);
 
-
-  const createBubbles = () => {
-    const bubbles = [];
-    const bubbleCount = 4; 
-
-    for (let i = 0; i < bubbleCount; i++) {
-      const size = gsap.utils.random(300, 600);
-      const top = gsap.utils.random(-50, 150) + "vh";
-      const left = gsap.utils.random(-20, 120) + "vw";
-      const rotation = gsap.utils.random(-45, 45);
-
-      const gradientOptions = [
-        "bg-gradient-to-tl from-teal-700 to-black",
-        "bg-gradient-to-br from-teal-800 to-black",
-        "bg-gradient-to-br from-teal-600 to-black",
-        "bg-teal-900",
-        "bg-teal-500",
-      ];
-      const gradient = gsap.utils.random(gradientOptions);
-
-      bubbles.push(
-        <div
-          key={i}
-          className={`absolute blob ${gradient}`}
-          style={{
-            width: `${size}px`,
-            height: `${size}px`,
-            top: top,
-            left: left,
-            filter: `blur(${gsap.utils.random(120, 250)}px)`,
-            opacity: gsap.utils.random(0.3, 0.6),
-            transform: `rotate(${rotation}deg)`,
-          }}
-        ></div>
-      );
-    }
-
-    return bubbles;
-  };
-
   return (
     <div ref={bgRef} className="fixed inset-0 bg-black -z-10 overflow-hidden">
       <div className="absolute inset-0 backdrop-blur-lg bg-black/80">
         <div className="absolute inset-0 bg-[url('/stars.png')] bg-cover opacity-10"></div>
       </div>
 
-
-      {createBubbles()}
+      {bubbles}
     </div>
   );
 };
